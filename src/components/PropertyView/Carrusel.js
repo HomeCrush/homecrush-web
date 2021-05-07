11628cc09714ead786b6f5c493d4af07baebc19d
andrea morales <amoralesp88@gmail.com>
2021-05-07 14:12:06 +0200

    cards views

--- src/components/PropertyView/Carrusel.js
+++ src/components/PropertyView/Carrusel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import { SliderData } from './SliderData';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
@@ -61,4 +61,85 @@ const ImageSlider = ({ slides }) => {
   );
 };
 
-export default ImageSlider;
+export default ImageSlider;*/
+
+import React, { useState } from 'react';
+import { SliderData } from './SliderData';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import CloseIcon from '@material-ui/icons/Close';
+import PropertyCard from './PropertyCard'
+import './Carrusel.css'
+import { Link as RouterLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import IconLabelButtons from './Bnt'
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import SimpleTooltips from './Options'
+
+
+const ImageSlider = ({ slides }) => {
+
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <Card>
+      <CardActionArea >
+      <ArrowBackIosIcon  className='left-arrow' onClick={prevSlide} />
+      <ArrowForwardIosIcon className='right-arrow' onClick={nextSlide} />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <div className="sliderImage" style={{
+                background: `url('${slide.image}')`
+              }}>
+              </div>
+            
+            )}
+          </div>
+        );
+      })}
+      </CardActionArea>
+      <CardActions>
+      <IconLabelButtons />
+      </CardActions>
+      <CardContent >
+      <Typography gutterBottom variant="h6" component="h6">
+      Titulo
+      </Typography>
+      <Typography variant="subtitle2" component="subtitle2">
+       Londres
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 specie
+            across all continents except Antarctica
+          </Typography>
+    </CardContent>
+      <SimpleTooltips />
+    </Card>
+        
+  );
+};
+
+export default ImageSlider;                                     
--- src/components/match/MatchCard.js
+++ src/components/match/MatchCard.js
@@ -1,4 +1,4 @@
-import Button from '@material-ui/core/Button';
+/*import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link as RouterLink } from 'react-router-dom';
 import Typography from "@material-ui/core/Typography";
@@ -44,4 +44,103 @@ export default function MatchCard ({ title, images, id }) {
         </Button>
       </div>
     );
+  }*/
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link as RouterLink } from 'react-router-dom';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 250,
+    marginBottom: theme.spacing(2),
+   
+  },
+  marginAutoContainer: {
+    display: 'flex',
+  },
+  marginAutoItem: {
+    margin: 'auto'
+  },
+  sub:{
+    marginBottom: theme.spacing(0),
+  },
+
+  alignItemsAndJustifyContent: {
+    width: 500,
+    height: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    objectFit:"cover",
+  },
+  icon:{
+    paddingRight: theme.spacing(1),
+  },
+  btnCenter:{
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn:{
+    background: "#44A1A0",
+    color: "#fff",
+    "&:hover": {
+  background: "#0D5C63",
+  color: "#fff"
   }
+}
+}));
+
+export default function MatchCard () {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.marginAutoContainer}>
+       <div className={classes.marginAutoItem}> 
+    <Card className={classes.root} >
+      <CardActionArea>
+        <CardMedia 
+         className={classes.image}
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" 
+          title="Contemplative Reptile"
+        />
+        <CardContent className={classes.sub}>
+          <Typography gutterBottom variant="h6" component="h6">
+          Titulo
+          </Typography>
+          <Typography variant="subtitle2" component="subtitle2">
+           Londres
+          </Typography>
+          <Typography variant="subtitle2" component="subtitle2">
+           Description
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.btnCenter}>
+        <Button className={classes.btn} size="small" color="primary" component={RouterLink} to={`/property/${1223}`}>
+         View
+        </Button>
+      </CardActions>
+    </Card>
+    </div>
+    </div>
+  );
+}
